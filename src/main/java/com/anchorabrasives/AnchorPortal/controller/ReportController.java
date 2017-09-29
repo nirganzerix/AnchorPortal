e687ccdfd8a96e718d0199545bb7c2d79f43a840
nirganzerix <nirganzerix@hotmail.com>
2017-09-29 02:55:08 -0500

    init pdf generation code

--- src/main/java/com/anchorabrasives/AnchorPortal/controller/ReportController.java
+++ src/main/java/com/anchorabrasives/AnchorPortal/controller/ReportController.java
@@ -1,16 +1,22 @@
 package com.anchorabrasives.AnchorPortal.controller;
  
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.anchorabrasives.AnchorPortal.model.ReportRequest;
 import com.anchorabrasives.AnchorPortal.model.ReportResponse;
+import com.anchorabrasives.AnchorPortal.service.ReportToPDFService;
  
 @Controller
 public class ReportController {
+	
+	@Autowired
+	ReportToPDFService reportToPDFService;
+	
 	@RequestMapping(value="/submitReport", method = RequestMethod.POST)
-		public ReportResponse submitReport(ReportRequest reportRequest) {
-			return new ReportResponse();
+		public void submitReport(ReportRequest reportRequest) {
+			//reportToPDFService.generateReportPDF();
 		}
 }
